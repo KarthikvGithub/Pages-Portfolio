a254ca4bf6da19db0a7a9b9346f9c2fb3fb2ff92
KarthikvGithub <valdaskarthikgoud@gmail.com>
2024-08-11 14:09:33 +0530

    first commit

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,151 @@
+import React, {useState} from 'react';
+import "./qualification.css"
+
+const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+  return (
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My Personal Journey</span>
+
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                  <div className={toggleState === 1 ? "qualification__button qualification__active button-flex" : "qualification__button button-flex"} onClick={() => toggleTab(1)}>
+                    <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+                    Education
+                </div>
+
+                <div className={toggleState === 2 ? "qualification__button qualification__active button-flex" : "qualification__button button-flex"} onClick={() => toggleTab(2)}>
+                    <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+                    Experience
+                </div>
+            </div>
+
+            <div className="qualification__sections">
+                <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">BTech (EE)</h3>
+                            <span className="qualification__subtitle">IIT Bhilai<br/>CGPA: 8.48</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> 2020 - 2024
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                        <div>
+                            <h3 className="qualification__title">Class 12</h3>
+                            <span className="qualification__subtitle">Sri Chaitanya<br/>Score: 98.8%</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> 2019 - 2020
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Class 10</h3>
+                            <span className="qualification__subtitle">Mahabubnagar Grammar<br/>GPA: 9.8</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> 2018
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    {/* <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                        <div>
+                            <h3 className="qualification__title">UX Expert</h3>
+                            <span className="qualification__subtitle">Spain - Institute</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> 2020 - 2024
+                            </div>
+                        </div>
+                    </div> */}
+                </div>
+
+
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Web Developer</h3>
+                            <span className="qualification__subtitle">Intern<br/>CodSoft</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> Jul - Aug 2023
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                        <div>
+                            <h3 className="qualification__title">Fest Committee</h3>
+                            <span className="qualification__subtitle">Volunteer<br/>IIT Bhilai</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> 2022
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Fest Committee</h3>
+                            <span className="qualification__subtitle">Volunteer<br/>IIT Bhilai</span>
+                            <div className="qualification__calender">
+                                <i class="uil uil-calendar-alt"></i> 2022
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Qualification;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import "./services.css";
+
+const Servies = () => {
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+  return (
+    <section className="services section" id="services">
+        <h2 className="section__title">Services</h2>
+        <span className="section__subtitle">What I Offer</span>
+
+        <div className="services__container container grid">
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title">
+                        Software <br /> Developer
+                    </h3>
+                </div>
+
+                <span className="services__button" onClick={() => toggleTab(1)}>
+                    View More
+                    <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+                <div className={ toggleState===1 ? "services__modal active-modal" : "services__modal"}>
+                    <div className="services__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">Developer</h3>
+                        <p className="services__modal-description">Passionate and motivated developer eager to provide innovative solutions to clients and companies.</p>
+
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Full-Stack Web Development</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">UI/UX Design</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Data Analysis & Visualization</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Network Programming</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Personal Webpage Development</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-edit services__icon"></i>
+                    <h3 className="services__title">
+                        Electrical <br /> Engineer
+                    </h3>
+                </div>
+
+                <span className="services__button" onClick={() => toggleTab(2)}>
+                    View More
+                    <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+                <div className={ toggleState===2 ? "services__modal active-modal" : "services__modal"}>
+                    <div className="services__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">Electrical Engineer</h3>
+                        <p className="services__modal-description">Dedicated electrical engineer committed to delivering innovative energy and data analysis solutions.</p>
+
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Piezoelectric Energy Generation</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Data Analysis for Sensor Applications</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">IMSI Catcher Development</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Embedded System Design</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Smart Device Integration</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+};
+
+export default Servies;
